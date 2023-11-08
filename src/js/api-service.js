a8bb52c91dd95825d2dd1867383ae6e521acdb9d
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-11-08 19:47:00 +0200

    ✅ Fix(JS) Update code

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -6,25 +6,42 @@ export default class ApiService {
     this.page = 1;
     this.PER_PAGE = 40;
   }
-
   async fetchGallery() {
     const axiosOptions = {
       method: 'get',
       url: 'https://pixabay.com/api/',
       params: {
         key: '34523545-f21683fd59bfc3e4e2549fe07',
-        q: this.searchQuery,
+        q: `${this.searchQuery}`,
         image_type: 'photo',
         orientation: 'horizontal',
-        page: this.page,
-        per_page: this.PER_PAGE,
+        safesearch: true,
+        page: `${this.page}`,
+        per_page: `${this.PER_PAGE}`,
       },
     };
+    try {
+      const response = await axios(axiosOptions);
 
-    const response = await axios(axiosOptions);
-    const data = response.data;
+      const data = response.data;
 
-    return data;
+      this.incrementPage();
+      return data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  resetEndOfHits() {
+    this.endOfHits = false;
   }
 
   get query() {
