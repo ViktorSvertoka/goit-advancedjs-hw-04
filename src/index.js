0617b22c5c99a16295a2d1f8a188749095415d37
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-11-08 02:42:34 +0200

    ⚠️ Fix(JS) Update code

--- src/index.js
+++ src/index.js
@@ -114,7 +114,7 @@ function onRenderGallery(elements) {
        <div class="info">
        <p class="info-item"><b>Likes</b>${likes}</p>
        <p class="info-item"><b>Views</b>${views}</p>
-       <p class "info-item"><b>Comments</b>${comments}</p>
+       <p class="info-item"><b>Comments</b>${comments}</p>
        <p class="info-item"><b>Downloads</b>${downloads}</p>
        </div>
        </div>`;
