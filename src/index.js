fd1d58b1b6653b9e17943dc97026f5049287f65d
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-11-10 22:38:10 +0200

    ⚠️ Refactor(Files) Update code

--- src/index.js
+++ src/index.js
@@ -18,12 +18,22 @@ let isLoading = false;
 searchForm.addEventListener('submit', onSearch);
 window.addEventListener('scroll', handleScroll);
 
-function onSearch(event) {
+async function onSearch(event) {
   event.preventDefault();
   const newQuery = event.currentTarget.searchQuery.value.trim();
+  if (newQuery === query) {
+    iziToast.info({
+      title: 'Info',
+      message: `The previous ${query} request has already been received, please enter a new search parameter.`,
+      position: 'topRight',
+      color: 'blue',
+    });
+
+    return;
+  }
   gallery.innerHTML = '';
 
-  if (newQuery === query || newQuery === '') {
+  if (!newQuery) {
     iziToast.warning({
       title: 'Warning',
       message: 'Please, fill the main field',
@@ -38,62 +48,61 @@ function onSearch(event) {
   page = 1;
   gallery.innerHTML = '';
 
-  apiService(query, page, perPage)
-    .then(({ data }) => {
-      if (data.totalHits === 0) {
-        iziToast.error({
-          title: 'Error',
-          message:
-            'Sorry, there are no images matching your search query. Please try again.',
-          position: 'topRight',
-          color: 'red',
-        });
-      } else {
-        onRenderGallery(data.hits);
-        simpleLightBox = new SimpleLightbox('.gallery a').refresh();
-        iziToast.success({
-          title: 'Success',
-          message: `Hooray! We found ${data.totalHits} images !!!`,
-          position: 'topRight',
-          color: 'green',
-        });
-      }
-    })
-    .catch(error => console.log(error))
-    .finally(() => {
-      searchForm.reset();
-      window.addEventListener('scroll', handleScroll);
+  try {
+    const { hits, totalHits } = await apiService(query, page, perPage);
+
+    if (totalHits === 0) {
+      iziToast.error({
+        title: 'Error',
+        message:
+          'Sorry, there are no images matching your search query. Please try again.',
+        position: 'topRight',
+        color: 'red',
+      });
+      return;
+    }
+
+    onRenderGallery(hits);
+    simpleLightBox = new SimpleLightbox('.gallery a').refresh();
+    iziToast.success({
+      title: 'Success',
+      message: `Hooray! We found ${totalHits} images !!!`,
+      position: 'topRight',
+      color: 'green',
     });
+  } catch (error) {
+    console.log(error);
+  } finally {
+    // searchForm.reset();
+    window.addEventListener('scroll', handleScroll);
+  }
 }
 
-function handleScroll() {
+async function handleScroll() {
   if (checkIfEndOfPage() && !isLoading) {
     isLoading = true;
     page += 1;
 
-    apiService(query, page, perPage)
-      .then(({ data }) => {
-        onRenderGallery(data.hits);
-        simpleLightBox.refresh();
+    try {
+      const { hits, totalHits } = await apiService(query, page, perPage);
+      onRenderGallery(hits);
+      simpleLightBox.refresh();
+      const totalPages = Math.ceil(totalHits / perPage);
 
-        const totalPages = Math.ceil(data.totalHits / perPage);
-
-        if (page >= totalPages) {
-          iziToast.info({
-            title: 'Info',
-            message:
-              "We're sorry, but you've reached the end of search results.",
-            position: 'topRight',
-            color: 'blue',
-          });
-
-          window.removeEventListener('scroll', handleScroll);
-        }
-      })
-      .catch(error => console.log(error))
-      .finally(() => {
-        isLoading = false;
-      });
+      if (page >= totalPages) {
+        iziToast.info({
+          title: 'Info',
+          message: "We're sorry, but you've reached the end of search results.",
+          position: 'topRight',
+          color: 'blue',
+        });
+        window.removeEventListener('scroll', handleScroll);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      isLoading = false;
+    }
   }
 }
 
--- src/js/api-service.js
+++ src/js/api-service.js
@@ -5,8 +5,8 @@ axios.defaults.baseURL = 'https://pixabay.com/api/';
 const KEY = '34523545-f21683fd59bfc3e4e2549fe07';
 
 async function apiService(query, page, perPage) {
-  const response = await axios.get(
+  const { data } = await axios.get(
     `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
   );
-  return response;
+  return data;
 }
